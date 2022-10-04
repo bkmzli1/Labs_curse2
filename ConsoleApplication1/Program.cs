591e97eac95e86102087001b011bf838684d0257
bkmzli1 <bkmzilbim@gmail.com>
2022-10-04 08:48:42 +0500

    6

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+   	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите желаемое количество повторений эксперимента: ");
+			int Nk = Convert.ToInt32(Console.ReadLine());
+
+			Random rnd = new Random();
+			double Iavg = 0;
+			int N = 59, t;
+
+			for (int n = 0; n < Nk; n++) // n - кол-во повторений эксперимента
+			{
+				Console.WriteLine("Эксперимент № " + (n + 1));
+				double[] px = new double[N];
+				double[,] pxy = new double[N, N];
+				double b = 1, Ix = 0, prob = 0, bi = 1;
+
+				for (int k = 0; k < N; k++)
+				{
+					px[k] = b * rnd.NextDouble(); // заполнение элементов массива случайными числами
+					b -= px[k]; // уменьшение верхнего предела с каждой итерацией
+					Ix += (-1) * px[k] * Math.Log(px[k], 2); // вычисление количества информации совокупности дискретных случайных сообщений
+					prob += px[k]; // проверка общей вероятности (должна ровняться 1)
+					Console.WriteLine("px[" + k + "]  =   " + px[k]);
+				}
+
+				Console.WriteLine();
+				Console.WriteLine("Матрица вероятности перехода со входа на выход P(X|Y)");
+				Console.WriteLine();
+				for (int i = 0; i < N; i++)
+				{
+					t = i;
+					bi = 1;
+					pxy[i, t] = 0.7 + 0.3 * rnd.NextDouble();
+					bi -= pxy[i, t];
+					string a = "";
+					for (int j = 0; j < N; j++)
+					{
+						if (i == j)
+						{
+							a += "P(X" + (i + 1) + "|" + "Y" + (j + 1) + ") = " + pxy[i, j] + ";   ";
+							continue;
+						}
+						else
+						{
+							pxy[i, j] = bi * rnd.NextDouble();
+							bi -= pxy[i, j];
+							a += "P(X" + (i + 1) + "|" + "Y" + (j + 1) + ") = " + pxy[i, j] + ";   ";
+						}
+						
+					}
+					Console.Write(a);
+					Console.WriteLine();
+				}
+
+				Console.WriteLine();
+				Console.WriteLine("Вероятности появления совокупности дискретных сообщений на выходе информационного устройства (P(Y))");
+				double[] yj = new double[N];
+				for (int i = 0; i < N; i++) // Вероятность появления совокупности дискретных сообщений на выходе информационного устройства P(y(j))
+				{
+					string l = "";
+					double Pyj = 0;
+					for (int j = 1; j < N; j++)
+					{
+						Pyj += px[j] * pxy[i, j];
+					}
+					yj[i] = Pyj;
+					l += "py" + (i + 1) + " = " + Pyj + "; ";
+					Console.WriteLine(l);
+				}
+
+				Console.WriteLine();
+				Console.WriteLine("Матрица вероятности  совместных событий P(X,Y)");
+				Console.WriteLine();
+				double[,] pxyi = new double[N, N]; // P (X , Y)
+				for (int i = 0; i < N; i++)
+				{
+					string s = "";
+					for (int j = 0; j < N; j++)
+					{
+						pxyi[i, j] = yj[j] * pxy[i, j];
+						s += "P(X" + (i + 1) + "," + "Y" + (j + 1) + ") = " + pxyi[i, j] + ";   ";
+					}
+					Console.WriteLine(s);
+					Console.WriteLine();
+				}
+
+				double Hx = 0;
+				for (int i = 0; i < N; i++)  // H(x)
+				{
+					Hx += (-1) * px[i] * Math.Log(px[i], 2);
+				}
+
+				double Hxy = 0; // H(x/y)
+				for (int i = 0; i < N; i++)
+				{
+					for (int j = 0; j < N; j++)
+					{
+						Hxy += (-1) * pxyi[i, j] * Math.Log(pxy[i, j], 2);
+					}
+				}
+
+				double Ixy = Hx - Hxy;
+
+				Console.WriteLine();
+				Console.WriteLine("Энтропия на входе информационного устройства: Н(х) = " + Hx);
+				Console.WriteLine("Условная энтропия выходного сообщения относительно входного: Н(х/у) = " + Hxy);
+				Console.WriteLine("Количество информации при неполной достоверности сообзений: I(x,y) = " + Ixy);
+				Console.WriteLine("===================================================");
+
+				Iavg += Ixy / 10; // Iср
+			}
+
+			Console.WriteLine("Среднее количество информации при неполной достоверности сообщений: Iср = " + Iavg);
+			Console.ReadKey(true);
+		}
+	}
+
+}
---
+++ lab6/Form3.Designer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace lab6
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(66, 123);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(468, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(58, 194);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(474, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(579, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(579, 197);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(454, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 598);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(802, 598);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1008, 598);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 54);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(579, 281);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(454, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(58, 278);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(474, 34);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(579, 330);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(454, 26);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(58, 327);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(474, 34);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(579, 381);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(454, 26);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(58, 378);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(474, 34);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 743);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form3";
+            this.Text = "form3";
+            this.VisibleChanged += new System.EventHandler(this.Form3_VisibleChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
