d70ae878215428e40a70ec06f4fbb06a0767bd19
bkmzli1 <bkmzilbim@gmail.com>
2022-10-04 08:47:32 +0500

    lab6

--- lab6/Form3.cs
+++ lab6/Form3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace lab6
@@ -10,12 +11,20 @@ namespace lab6
             InitializeComponent();
         }
 
+        private Random Random = new Random();
+
         internal void button2_Click(object sender, EventArgs e)
         {
             Owner.Text = textBox1.Text;
             Owner.OwnedForms[0].Text = textBox2.Text;
             Owner.OwnedForms[1].Text = textBox2.Text;
+            Owner.OwnedForms[1].Text = textBox2.Text;
+            Owner.OwnedForms[1].Width = Convert.ToInt32(textBox3.Text);
+            Owner.OwnedForms[1].Height = Convert.ToInt32(textBox4.Text);
+            Owner.OwnedForms[1].BackColor = Color.FromArgb(Random.Next(0, Convert.ToInt32(textBox5.Text)),
+                Random.Next(0, Convert.ToInt32(textBox5.Text)), Random.Next(0, Convert.ToInt32(textBox5.Text)));
         }
+
         private void Form3_VisibleChanged(object sender, EventArgs e)
         {
             if (Visible)
@@ -24,12 +33,14 @@ namespace lab6
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-           
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
         }
     }
 }
